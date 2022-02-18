cc6c138ed5a8c0cc5a1c169bc677533de0b2818c
Karol  Soares <karolina.soares@sumup.com>
2022-02-17 22:13:48 -0300

    Refatorando componentes footer e login

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Box, Grid } from '@material-ui/core';
+import './Footer.css';
 
 function Footer() {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#03247c", height: "120px" }}>
+                    <Box className='box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h6" align="center" gutterBottom style={{ color: "white" }}>Conheça minha rede: </Typography>
+                            <Typography variant="h6" align="center" gutterBottom className='texto'>Conheça minha rede: </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://www.linkedin.com/in/karol-soares/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                                <LinkedInIcon className='rede' />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#f78a4a", height: "60px" }}>
+                    <Box className='box2'>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2022 </Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className='texto' >© 2022 </Typography>
                         </Box>
                         <Box>
                         
-                            <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">Karolina Soares</Typography>
+                            <Typography variant="subtitle2" gutterBottom className='texto' align="center">Karolina Soares</Typography>
                         
                         </Box>
                     </Box>
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -10,7 +10,7 @@ function Login() {
             <Grid alignItems='center' xs={6} >
                 <Box paddingX={20}>
                     <form>
-                        <Typography variant="h3" gutterBottom color='textPrimary' component='h3' align='center' style={{fontWeight: 'bold'}}>Entrar</Typography>
+                        <Typography variant="h3" gutterBottom color='textPrimary' component='h3' align='center' className="textolog">Entrar</Typography>
                         <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
                         <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
                         <Box marginTop={2} textAlign='center'>
@@ -25,7 +25,7 @@ function Login() {
                         <Box marginRight={1}>
                             <Typography variant='subtitle1' gutterBottom align='center'> Não tem uma conta?</Typography>
                         </Box>
-                        <Typography variant='subtitle1' gutterBottom align='center' style={{fontWeight: 'bold'}}>Cadastre-se</Typography>
+                        <Typography variant='subtitle1' gutterBottom align='center' className="textolog">Cadastre-se</Typography>
                     </Box>
                 </Box>
             </Grid>
